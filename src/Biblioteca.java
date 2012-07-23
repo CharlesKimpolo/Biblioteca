f122bc13257a6e7ee5f30faf0db8dccfe85d0602
Charles <clmkimpo@thoughtworks.com>
2012-07-23 15:15:06 +0200

    Implementation of the first requirement

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,2 +1,8 @@
 public class Biblioteca {
+
+    public static void main(String[] args){
+        System.out.println("Welcome to Biblioteca");
+    }
+
+
 }
--- test/BibliotecaTest.java
+++ test/BibliotecaTest.java
@@ -1,2 +1,31 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
 public class BibliotecaTest {
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    public final PrintStream out = new PrintStream(outContent);
+
+    @Before
+    public void setUp() throws Exception {
+    System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(null);
+    }
+
+    @Test
+    public void shouldDisplayWelcomeMessage() throws Exception {
+        Biblioteca.main(null);
+        assertEquals("Welcome to Biblioteca", outContent.toString().trim());
+    }
+
 }
+
